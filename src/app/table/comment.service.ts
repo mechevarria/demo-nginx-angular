7b2380a1815d40c976ba2a76865865ca29aa7dfa
Mike Echevarria <mechevar@redhat.com>
2019-06-12 11:33:47 -0400

    use paging for service call to jsonplaceholder

--- src/app/table/comment.service.ts
+++ src/app/table/comment.service.ts
@@ -1,14 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Comment} from './comment';
-import {catchError} from 'rxjs/operators';
-import {MessageService} from '../message/message.service';
-import {Observable, of} from 'rxjs/index';
-
-
-const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpParams } from '@angular/common/http';
+import { Comment } from './comment';
+import { catchError } from 'rxjs/operators';
+import { MessageService } from '../message/message.service';
+import { Observable, of } from 'rxjs/index';
 
 
 @Injectable({
@@ -20,8 +15,16 @@ export class CommentService {
   constructor(private messageService: MessageService, private http: HttpClient) {
   }
 
-  getComments(): Observable<Comment[]> {
-    return this.http.get<Comment[]>(this.url)
+  // X-Total-Count is returned in the header so full response is needed
+  getComments(page: number, limit: number): Observable<HttpResponse<Comment[]>> {
+    const options: any = {
+      observe: 'response',
+      params: new HttpParams()
+        .append('_limit', limit.toString())
+        .append('_start', ((page - 1) * limit).toString()),
+    };
+
+    return this.http.get<Comment[]>(this.url, options)
       .pipe(
         catchError(error => {
           this.messageService.error(`getComments() ${error.message}`);
