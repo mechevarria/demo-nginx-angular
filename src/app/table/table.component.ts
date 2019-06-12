20616b4e72f7fc2b6a8a424f629c733c1d410bfe
Mike Echevarria <mechevar@redhat.com>
2019-06-12 11:34:14 -0400

    remove angular datatables and use ngx-pager with service options

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,73 +1,41 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MessageService } from '../message/message.service';
+import { Component, OnInit } from '@angular/core';
 import { CommentService } from './comment.service';
 import { Comment } from './comment';
-import { Subject } from 'rxjs';
-import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html'
 })
-export class TableComponent implements OnDestroy, OnInit, AfterViewInit {
-  @ViewChild(DataTableDirective)
-  dtElement: DataTableDirective;
+export class TableComponent implements OnInit {
 
-  dtOptions: DataTables.Settings;
-  comments: Comment[];
-  dtTrigger: Subject<any>;
+  comments: Comment[] = new Array();
+  count: number = 0;
+  limit: number = 10;
+  page: number = 1;
 
-  constructor(private messageService: MessageService, private commentService: CommentService) {
-    this.dtOptions = {};
-    this.comments = new Array();
-    this.dtTrigger = new Subject();
+  constructor(private commentService: CommentService) {
   }
 
   load(): void {
-    this.commentService.getComments().subscribe(res => {
-      this.comments = res;
-      this.rerender();
-
-      if (this.comments != null) {
-        this.messageService.success(`Successfully loaded ${this.comments.length} comments from service`);
+    this.commentService.getComments(this.page, this.limit).subscribe(res => {
+      if (res !== null) {
+        this.comments = res.body;
+        this.count = parseInt(res.headers.get('x-total-count'));
       }
     });
   }
 
-  rerender(): void {
-    if (this.dtElement.dtInstance) {
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        // Destroy the table first
-        dtInstance.destroy();
-
-        // Call the dtTrigger to rerender again
-        this.dtTrigger.next();
-      });
-    } else {
-      this.dtTrigger.next();
-    }
+  pageChanged(event: any): void {
+    this.page = event.page;
+    this.load();
   }
 
-  clear(): void {
-    this.comments = [];
-    this.rerender();
-    this.messageService.info('Cleared data');
-  }
-
-  ngOnDestroy(): void {
-    // Do not forget to unsubscribe the event
-    this.dtTrigger.unsubscribe();
+  limitChanged(): void {
+    this.page = 1;
+    this.load();
   }
 
   ngOnInit() {
-    this.dtOptions = {
-      pagingType: 'simple',
-      responsive: true,
-      pageLength: 10
-    };
-  }
-
-  ngAfterViewInit() {
-    this.rerender();
+    this.load();
   }
 }
