df46cdb80ed3f78afd22e4b47053c1754cc4234f
Mike Echevarria <michael.echevarria@sapns2.com>
2019-10-13 14:15:03 -0400

    add MapBox draw controls and update logos

--- src/app/app-map/app-map.component.ts
+++ src/app/app-map/app-map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppMapService } from './app-map.service';
 import { MessageService } from '../message/message.service';
+import * as MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-app-map',
@@ -11,6 +13,11 @@ export class AppMapComponent implements OnInit {
   geojson: any;
 
   constructor(private appMapService: AppMapService, private messageService: MessageService) {
+  }
+
+  loadMap(map: Map) {
+    const draw = new MapboxDraw();
+    map.addControl(draw, 'top-right');
   }
 
   ngOnInit() {
