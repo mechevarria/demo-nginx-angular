b4a0d491ee40cc0aeccf1e88a453da7f195e2872
mechevarria <mike.echevarria@gmail.com>
2020-03-23 19:14:59 -0400

    add name to docker run scripts

--- docker-run-dev.sh
+++ docker-run-dev.sh
@@ -2,6 +2,7 @@
 
 docker run \
   -p 80:80 \
+  --name nginx-angular \
   --env MAPBOX_TOKEN=${MAPBOX_TOKEN} \
   --mount type=bind,source=${PWD}/dist,target=/usr/share/nginx/html \
   quay.io/mechevarria/nginx-angular
--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,6 @@
 
 docker run \
     -p 80:80 \
+    --name nginx-angular \
     --env MAPBOX_TOKEN=${MAPBOX_TOKEN} \
     quay.io/mechevarria/nginx-angular
