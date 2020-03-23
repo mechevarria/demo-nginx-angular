a1f576f317b8c93781b692a0e9d89b53735e0fce
mechevarria <mike.echevarria@gmail.com>
2020-03-23 19:35:31 -0400

    remove name param

--- docker-run-dev.sh
+++ docker-run-dev.sh
@@ -2,7 +2,6 @@
 
 docker run \
   -p 80:80 \
-  --name nginx-angular \
   --env MAPBOX_TOKEN=${MAPBOX_TOKEN} \
   --mount type=bind,source=${PWD}/dist,target=/usr/share/nginx/html \
   quay.io/mechevarria/nginx-angular
--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,5 @@
 
 docker run \
     -p 80:80 \
-    --name nginx-angular \
     --env MAPBOX_TOKEN=${MAPBOX_TOKEN} \
     quay.io/mechevarria/nginx-angular
