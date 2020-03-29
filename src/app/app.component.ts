877f5ab433b05c7cdd2d358bf05474df67debdc6
mechevarria <mike.echevarria@gmail.com>
2020-03-29 09:31:15 -0400

    use ngClass instead of renderer2 and manage mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { SidebarService } from './sidebar/sidebar.service';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
@@ -6,27 +6,34 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  @ViewChild('sidebar', { read: ElementRef })
-  private sidebar: ElementRef;
+export class AppComponent implements AfterViewInit {
+  isMin: boolean = false;
+  isHid: boolean = false;
 
   constructor(private sidebarService: SidebarService, private deviceService: DeviceDetectorService) {
     this.sidebarService.toggleMin$.subscribe(() => {
-      this.sidebar.nativeElement.classList.toggle('c-sidebar-minimized');
+      this.isMin = !this.isMin;
       this.triggerResize();
     });
     this.sidebarService.toggleHide$.subscribe(() => {
-      this.sidebar.nativeElement.classList.toggle('c-sidebar-show');
+      this.isHid = !this.isHid;
       this.triggerResize();
     });
   }
 
   triggerResize(): void {
-    // triggering this event so that the mapbox api will auto resize the map
+    // triggering this event so that the mapbox will auto resize the map
     if (this.deviceService.isDesktop()) {
       setTimeout(() => {
         window.dispatchEvent(new Event('resize'));
       }, 200);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    // hide sidebar by default on mobile
+    if (this.deviceService.isMobile()) {
+      this.sidebarService.toggleHide$.next();
     }
   }
 }
