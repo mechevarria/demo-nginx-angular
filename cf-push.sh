aa1139b2ea8f38fcf542cbf321f129331af1b562
mechevarria <mike.echevarria@gmail.com>
2020-03-29 21:31:42 -0400

    add environment variable setting to pushing application

--- cf-push.sh
+++ cf-push.sh
@@ -3,4 +3,9 @@
 cf push nginx-angular \
     -m 64M \
     -k 256M \
-    --docker-image quay.io/mechevarria/nginx-angular
+    --docker-image quay.io/mechevarria/nginx-angular \
+    --no-start
+
+cf se nginx-angular MAPBOX_TOKEN $MAPBOX_TOKEN
+
+cf start nginx-angular
