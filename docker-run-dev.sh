1e26bb80a6bdd731c301f2ab4b5a5e04a7dda9d0
Mike Echevarria <mike.echevarria@gmail.com>
2020-07-06 16:17:33 -0400

    upgrade Angular 10, fix build warning, dev use of ng serve

--- docker-run-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  -p 80:80 \
-  --env MAPBOX_TOKEN=$MAPBOX_TOKEN \
-  --env KEYCLOAK=$KEYCLOAK \
-  --env KEYCLOAK_URL=$KEYCLOAK_URL \
-  --mount type=bind,source=${PWD}/dist,target=/usr/share/nginx/html \
-  quay.io/mechevarria/demo-nginx-angular
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER, DoBootstrap, ApplicationRef } from '@angular/core';
+import { NgModule, DoBootstrap, ApplicationRef } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
